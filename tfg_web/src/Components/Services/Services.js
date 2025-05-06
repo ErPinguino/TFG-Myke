1c673b7f32af3b193bed7af9c734b6db7d6f1ed2
Myke <michaeheno23j@gmail.com>
2025-05-06 11:03:15 +0200

    front mejorado, solo estructuras

--- tfg_web/src/Components/Services/Services.js
+++ tfg_web/src/Components/Services/Services.js
@@ -1,13 +1,18 @@
-// src/Services/Services.js
 import axios from 'axios';
 
+// Apunta al mismo mapping que tu controlador Spring Boot:
 const API_BASE = '/minecraftProject/structures';
 
 export const structureService = {
+  // GET /minecraftProject/structures/types
   getAvailableStructures: () =>
-      axios.get(`${API_BASE}/types`).then(res => res.data),
+      axios
+          .get(`${API_BASE}/types`)
+          .then(res => res.data),
 
-  findStructures: (input) =>
-      // input.seed es STRING, x,z,radius,count números
-      axios.post(`${API_BASE}/search`, input).then(res => res.data),
+  // POST /minecraftProject/structures/search
+  findStructures: input =>
+      axios
+          .post(`${API_BASE}/search`, input)
+          .then(res => res.data),
 };
