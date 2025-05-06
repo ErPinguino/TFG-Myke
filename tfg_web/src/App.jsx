cf1760878cf9f4363a249bac938979e087d40ab9
Myke <michaeheno23j@gmail.com>
2025-05-06 11:27:38 +0200

    front mejorado, solo estructuras

--- tfg_web/src/App.jsx
+++ tfg_web/src/App.jsx
@@ -1,23 +1,12 @@
-import React, { useState } from 'react';
-import BiomeSearch from './Components/BiomeSearch/BiomeSearch';
+// src/App.jsx
+import React from 'react';
 import StructureSearch from './Components/StructureSearch/StructureSearch';
 import './App.css';
 
 export default function App() {
-  const [mode, setMode] = useState('biome');
   return (
-    <div className="App">
-      <div className="mode-switcher">
-        <button onClick={() => setMode('biome')}
-                className={mode === 'biome' ? 'active' : ''}>
-          Find Biome
-        </button>
-        <button onClick={() => setMode('structure')}
-                className={mode === 'structure' ? 'active' : ''}>
-          Find Structure
-        </button>
+      <div className="App">
+        <StructureSearch />
       </div>
-      {mode === 'biome' ? <BiomeSearch /> : <StructureSearch />}
-    </div>
   );
 }
